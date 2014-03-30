7540e3302bdbefcabd3506e9298d6489d4741e87
Siddhi Chogle <siddhi.chogle@sjsu.edu>
2014-03-29 23:05:12 -0700

    -initial commit on worklight project

---
+++ apps/HelloWorklight/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":2189339619,"date":1396156276204,"machine":"Siddhis-MacBook-Pro.local"};
+/* Date: Sat Mar 29 22:11:16 PDT 2014 */
---
+++ apps/HelloWorklight/iphone/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3854452116,"date":1396156274157,"machine":"Siddhis-MacBook-Pro.local"};
+/* Date: Sat Mar 29 22:11:14 PDT 2014 */
